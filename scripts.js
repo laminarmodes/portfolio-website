d0d8b3f6c6a74dabef60f407628ff21057da4929
laminarmodes <anya.traille@gmail.com>
2021-11-23 02:47:10 +0800

    Back button added

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ $('.menu-toggle').click(function(){
     $(".navigation-list").toggleClass("mobile-nav");
     $(this).toggleClass("is-active");
  });
+
+ function goBack() {
+    window.history.back();
+}
